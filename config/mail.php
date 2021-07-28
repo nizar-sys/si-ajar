f16313ac2354c01f9f51e9b2266710eab3ffdef7
nizar-sys <raspbery132@gmail.com>
2021-07-28 16:47:06 +0700

    change mailer

--- config/mail.php
+++ config/mail.php
@@ -36,11 +36,11 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+            'host' => env('MAIL_HOST', 'smtp.gmail.com'),
             'port' => env('MAIL_PORT', 587),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-            'username' => env('MAIL_USERNAME'),
-            'password' => env('MAIL_PASSWORD'),
+            'username' => env('MAIL_USERNAME', 'nizarid04@gmail.com'),
+            'password' => env('MAIL_PASSWORD', 'amocboxnuluyaxmj'),
             'timeout' => null,
             'auth_mode' => null,
         ],
@@ -84,8 +84,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'muhamadnizar@smkwikrama.sch.id'),
+        'name' => env('MAIL_FROM_NAME', 'Si - Ajar'),
     ],
 
     /*
